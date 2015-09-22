5f982c0aace78513dd32f92ed472ee2ff6a7aca6
Ucup Timposu <ucup.timposu@gmail.com>
2015-09-22 23:34:15 +0800

    upload mvn project

---
+++ src/main/java/com/timposu/springboot/App.java
@@ -0,0 +1,13 @@
+package com.timposu.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
