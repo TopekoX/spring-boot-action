f6b54904c6fd45138cdc8714b1c087fc9785787a
Ucup Timposu <ucup.timposu@gmail.com>
2015-09-23 23:33:39 +0800

    Konek database mysql

--- src/main/java/com/timposu/springboot/service/ProductRepository.java
+++ src/main/java/com/timposu/springboot/service/ProductRepository.java
@@ -3,7 +3,9 @@ package com.timposu.springboot.service;
 import org.springframework.data.repository.CrudRepository;
 
 import com.timposu.springboot.domain.Product;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
 	
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-insert into m_product(code, name, price) values ('P-001','Product 1',3450.00)
-insert into m_product(code, name, price) values ('P-002','Product 2',5214.00)
-insert into m_product(code, name, price) values ('P-003','Product 3',35666.00)
+insert into m_product(code, name, price) values ('P-001','Product 1',3450.00);
+insert into m_product(code, name, price) values ('P-002','Product 2',5214.00);
+insert into m_product(code, name, price) values ('P-003','Product 3',35666.00);
