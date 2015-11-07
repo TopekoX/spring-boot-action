d1f5bd027d5225330f9a410d6740b9159d34be8a
Ucup Timposu <ucup.timposu@gmail.com>
2015-11-07 10:27:17 +0800

    sukses backend

--- src/main/java/com/timposu/springboot/controller/ProductController.java
+++ src/main/java/com/timposu/springboot/controller/ProductController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.timposu.springboot.domain.Product;
 import com.timposu.springboot.service.ProductRepository;
+import java.net.URI;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.util.UriTemplate;
 
 @RestController
 @RequestMapping("/api")
@@ -28,5 +35,32 @@ public class ProductController {
         @RequestMapping(value = "/product/{id}", method=RequestMethod.GET)
         public Product findById(@PathVariable Long id){
             return productRepository.findOne(id);
+        }
+        
+        @RequestMapping(value = "/product", method = RequestMethod.POST)
+        @ResponseStatus(HttpStatus.CREATED)
+	public void create(@RequestBody Product product, HttpServletRequest request, 
+                HttpServletResponse response ){
+            productRepository.save(product);
+            Long newId = product.getId();
+            String requestUrl = request.getRequestURL().toString();
+            URI uri = new UriTemplate("{requestUrl}/{id}").expand(requestUrl, newId);
+            response.setHeader("Location", uri.toASCIIString());
+        }
+        
+        @RequestMapping(value = "/product/{id}", method = RequestMethod.PUT)
+        @ResponseStatus(HttpStatus.OK)
+	public void update(@PathVariable Long id, @RequestBody Product product){
+            Product px = productRepository.findOne(id);
+            if (px == null){
+                throw new IllegalStateException("No product id :" + id);
+            }
+            product.setId(id);
+            productRepository.save(product);
+        }
+        
+        @RequestMapping(value = "/product/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable Long id){
+            productRepository.delete(id);
         }
 }
